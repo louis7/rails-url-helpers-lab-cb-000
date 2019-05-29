a7d1353326ff1c10de758aefe6c1dfb7c74b22f2
louis baptiste <louisvis@ymail.com>
2019-05-29 00:38:35 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: :index
+  get '/students/:id', to: 'students#show', as: 'student'
 
   # This show route can be refactored into the above resources method call, like so:
   # resources :students, only: [:index, :show]
